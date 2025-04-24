7b26d4f47268371aa90afe8664ef97a4971a255b
zenoworld <subhromajumder52@gmail.com>
2025-04-24 18:07:45 +0530

    first commit

---
+++ travel_front/src/imageGallery/Masonygallery.jsx
@@ -0,0 +1,43 @@
+import React from 'react'
+import Masonry, { ResponsiveMasonry } from 'react-responsive-masonry'
+import galleryImg from './galleryImg'
+import { motion } from 'framer-motion'
+
+import '../style/home.css'
+
+const fadeImg = {
+  hidden: { opacity: 0, x: -300 , rotateX: 180},
+  visible: {
+    opacity: 1,
+    x: 0,
+    rotateX: 0,
+  },
+}
+const Masonygallery = () => {
+  return <>
+    <ResponsiveMasonry columnsCountBreakPoints={{ 350: 1, 768: 3, 992: 4 }}>
+      <Masonry gutter='1rem'>
+        {
+          galleryImg.map((item, index) => (
+            <motion.div
+              variants={fadeImg}
+              initial="hidden"
+              whileInView="visible"
+              transition={{ duration: 0.15 * (galleryImg.length - index), ease: 'easeInOut' }}
+              className='container1' key={index}>
+              <div className='cards'>
+                <img src={item.src} alt='' width={450} height={410} />
+              </div>
+              <div className='content'>
+                <h1>{item.title}</h1>
+                <p>{item.description}</p>
+              </div>
+            </motion.div>
+          ))
+        }
+      </Masonry>
+    </ResponsiveMasonry>
+  </>
+}
+
+export default Masonygallery
---
+++ travel_front/src/imageGallery/galleryImg.js
@@ -0,0 +1,22 @@
+import galleryImg01 from '../data/image/gallery-01.jpg'
+import galleryImg02 from '../data/image/gallery-02.jpg'
+import galleryImg03 from '../data/image/gallery-03.jpg'
+import galleryImg04 from '../data/image/gallery-04.jpg'
+import galleryImg05 from '../data/image/gallery-05.jpg'
+import galleryImg06 from '../data/image/gallery-06.jpg'
+import galleryImg07 from '../data/image/gallery-07.jpg'
+import galleryImg08 from '../data/image/gallery-08.png'
+
+const galleryImg=[
+    { src: galleryImg01, title: "Image Title 1", description: "Description for Image 1" },
+    { src: galleryImg02, title: "Image Title 2", description: "Description for Image 2" },
+    { src: galleryImg03, title: "Image Title 3", description: "Description for Image 3" },
+    { src: galleryImg04, title: "Image Title 4", description: "Description for Image 4" },
+    { src: galleryImg05, title: "Image Title 5", description: "Description for Image 5" },
+    { src: galleryImg06, title: "Image Title 6", description: "Description for Image 6" },
+    { src: galleryImg07, title: "Image Title 7", description: "Description for Image 7" },
+    { src: galleryImg08, title: "Image Title 8", description: "Description for Image 8" },
+    
+
+]
+export default galleryImg;
